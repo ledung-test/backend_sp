1cdcf1bbb7b118d69388e60d3c2e9f67fefafcd5
ledung-test <110715254+ledung-test@users.noreply.github.com>
2024-03-09 09:27:09 +0700

    Commit

---
+++ src/main/java/com/example/backend_sp/BackendSpApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backend_sp/BackendSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_sp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
